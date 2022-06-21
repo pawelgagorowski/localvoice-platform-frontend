134d195773660947476b6e6f37d95ee50a24ac5d
pawel <pawelgagorowski026@gmail.com>
2022-06-21 11:52:37 +0200

    remove auth directory

--- src/auth/authenticate.js
+++
@@ -1,8 +0,0 @@
-export default (to, from, next) => {
-  if (localStorage.getItem('userInfo') != null && localStorage.getItem('userInfo').length > 0) {
-    next();
-  } else {
-    localStorage.removeItem('userInfo');
-    next('/app/sessions/sign-in-two');
-  }
-};
