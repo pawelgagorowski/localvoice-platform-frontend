3a1a06a7701817b627cea7af3146af7bed0bdfc0
Paweł Gągorowski <pawelgagorowski026@gmail.com>
2022-12-27 00:22:02 +0100

    remove and upgrade dep

--- src/app/shared/vendors/index.ts
+++ src/app/shared/vendors/index.ts
@@ -13,8 +13,8 @@ export const VendorsPlugin: PluginObject<void> = {
     // async load fonts
     WebFontLoader.load({
       google: {
-        families: ['Archivo:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap'],
-      },
+        families: ['Archivo:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap']
+      }
     });
-  },
+  }
 };
--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,10 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable @typescript-eslint/no-var-requires */
+
+const { defineConfig } = require('@vue/cli-service');
 const path = require('path');
-const CircularDependencyPlugin = require('circular-dependency-plugin');
 
-const isDev = process.env.NODE_ENV !== 'production';
-
-module.exports = {
+module.exports = defineConfig({
   pages: {
     index: {
       entry: 'src/main.ts',
@@ -13,47 +12,18 @@ module.exports = {
       filename: 'index.html'
     }
   },
+  publicPath: '/',
 
   devServer: {
-    clientLogLevel: 'warning',
     hot: true,
-
     open: process.platform === 'darwin',
-    host: '0.0.0.0',
+    host: 'localhost',
     port: 8000,
     https: true,
-    hotOnly: false,
-
-    contentBase: 'dist',
-    compress: true,
-    // open: true,
-    overlay: {
-      warnings: false,
-      errors: true
-    },
-    publicPath: '/',
-    quiet: true,
-    watchOptions: {
-      poll: false,
-      ignored: /node_modules/
-    },
-    disableHostCheck: true
+    compress: true
   },
   chainWebpack: (config) => {
     config.resolve.alias.set('~app', path.resolve('./src/app'));
-
-    // /**
-    //  * disable cssnano calc buggy optimization
-    //  * @see https://github.com/cssnano/cssnano/issues/742
-    //  */
-    // if (!isDev) {
-    //   config.plugin('optimize-css').tap(([options]) => {
-    //     options.cssnanoOptions.preset[1].calc = false;
-    //     return [options];
-    //   });
-    // }
-
-    // config.plugin('circular-dependency').use(CircularDependencyPlugin, [{ exclude: /node_modules/ }]);
 
     if (process.argv.some((arg) => arg.includes('report'))) {
       config.optimization.concatenateModules(false);
@@ -61,4 +31,4 @@ module.exports = {
   },
   lintOnSave: false,
   transpileDependencies: ['vuetify']
-};
+});
